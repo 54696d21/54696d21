1844b1cc4cc76b43e9f47585ca8f68ccd537683f
54696d21 <tim@timgabriel.de>
2021-04-18 12:01:18 +0200

    zwischenstand 2

--- Contributor.py
+++ Contributor.py
@@ -1,4 +1,4 @@
-class Contributor:
+class Contributor(object):
     # def __init__(self) -> None:
     def __init__(self, **kwargs):
         # self.val2 = kwargs.get('val2', "default value")
@@ -12,10 +12,65 @@ class Contributor:
         self.username = kwargs.get("username", None)
         self.validatedClipsBeginning = kwargs.get("validatedClipsBeginning", None)
         self.recordedClipsBeginning = kwargs.get("recordedClipsBeginning", None)
+        self.clientHash = kwargs.get("clientHash", None)
         self.currentRecordedClips = None
         self.currentValidatedClips = None
-        self.clientHash = kwargs.get("clientHash", None)
+        self.validatedClipsDelta = None
+        self.recordedClipsDelta = None
+        self.competitionScore = None
 
+    def populateDeltas(self) -> None:
+        self.validatedClipsDelta = self.currentValidatedClips - self.validatedClipsBeginning
+        self.recordedClipsDelta = self.currentRecordedClips - self.recordedClipsBeginning
 
+    def populateScore(self) -> None:
+        self.competitionScore = (
+            0.5*self.validatedClipsDelta) + self.recordedClipsDelta
+
+    def test4(self) -> None:
+        # print(self)
+        for attr, value in self.__dict__.iteritems():
+            yield attr, value
+        # for i in vars(self):
+        # for i in self.__dict__.iteritems():
+        #     print(i)
+
+    def __repr__(self):
+        return str(self.__dict__)
+        # print("hi")
+        # for i in self:
+        #     print(i)
+    #     return "<Contributor a:%s b:%s>" % (self.a, self.b)
+
+    def __str__(self):
+        return str(self.__dict__)
+        # return "<Contributor username:%s username:%s>" % (self.username, self.username)
+        # return dict(self)
+        # for i in self:
+        #     print(i)
+        # return "tper"
         # self.validatedApiResonseContent = None
         # self.recordedApiResonseContent = None
+
+
+class Mensch:
+    def __init__(self):
+        pass
+
+    def test3(self):
+        print("ok")
+
+
+if __name__ == "__main__":
+    w = Contributor(username="54696d21", clientHash="0272ed18c2dfde45ceb61d5f0dd34fcb8026d55cb6876339dc3762c45bc58699", validatedClipsBeginning=11, recordedClipsBeginning=12)
+    w.test4()
+    # # print(f)
+    # f = Contributor(username="54696d21")
+    # p = Contributor(username="54696d21",
+    #                 clientHash="0272ed18c2dfde45ceb61d5f0dd34fcb8026d55cb6876339dc3762c45bc58699", validatedClipsBeginning=11)
+    # # f.populateScore()
+    # f.test2()
+    # f.peter()
+
+    # g = Mensch()
+    # g.test3()
--- pullData3.py
+++ pullData3.py
@@ -2,18 +2,18 @@ import requests
 import json
 import datetime
 import os
+import logging
 import time
-from Contributor import COn
+from Contributor import Contributor
+from contributorsCompetition import USERS3
 
 URL_VALIDATED = "https://commonvoice.mozilla.org/api/v1/de/clips/votes/leaderboard?cursor=[1,23565]"
 URL_RECORDED = "https://commonvoice.mozilla.org/api/v1/de/clips/leaderboard?cursor=[1,23565]"
 
 
-
-
 class Data:
     def __init__(self) -> None:
-    # def __init__(self, **kwargs):
+        # def __init__(self, **kwargs):
         self.validatedApiResonseContent = None
         self.recordedApiResonseContent = None
 
@@ -29,11 +29,54 @@ class Data:
         for i in self.recordedApiResonseContent:
             i["avatarClipUrl"] = None
             i["avatar_url"] = None
-    
+
+    def buildDashboard(self) -> None:
+        pass
+        # print('ok')
+        for contributor in USERS3:
+            # print(i.username)
+            # repr(i)
+            for j in self.recordedApiResonseContent:
+                # print(i)
+                if j["username"] == contributor.username:
+                    # print(j["username"])
+                    contributor.currentRecordedClips = j.get("total")
+            for j in self.validatedApiResonseContent:
+                        # print(i)
+                if j["username"] == contributor.username:
+                    # print(j["username"])
+                    contributor.currentValidatedClips = j.get("total")
+                    # i.currentValidatedClips
+                    # i.currentValidatedClips
+            contributor.populateDeltas()
+            contributor.populateScore()
+            # for i in data:
+            # user = i["username"]
+            # if user in contributorsCompetition.USERS:
+            #     # user contributorsCompetition.USERS2[user]
+            #     for j in contributorsCompetition.USERS2:
+            #         if j["user"] == user:
+            #             oldContrib = j["contrib"]
+            #             delta = i["total"] - oldContrib
+            #             o.append({'user': user, 'delta': delta})
+
+    def buildRanking(self) -> None:
+        # sorted()
+        # print(sorted(USERS3, key=lambda USERS3.: o['delta'], reverse=True))
+        sortedList = sorted(USERS3, key=lambda contributor: contributor.competitionScore, reverse=True)
+        print(sortedList)
+
 
 
 if __name__ == "__main__":
     data = Data()
     data.fetchDataFromApi()
+    data.buildDashboard()
+    # for user in USERS3:
+    #     print(user.username)
+    #     print(user.currentRecordedClips)
+    #     print(user.currentValidatedClips)
+    #     print("-----")
+    data.buildRanking()
     # print(data.recordedApiResonseContent)
     # print(data.validatedApiResonseContent)
