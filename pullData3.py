e9da7a996b7b0771e866783208580cc7b3b70230
54696d21 <tim@timgabriel.de>
2021-04-18 10:06:12 +0200

    rewrite in OOP

---
+++ pullData3.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import datetime
+import os
+import time
+
+
+class Contributor:
+    def __init__(self) -> None:
+        self.username
+        self.validatedClipsBeginning
+        self.recordedClipsBeginning
+        self.currentRecordedClips
+        self.currentValidatedClips
